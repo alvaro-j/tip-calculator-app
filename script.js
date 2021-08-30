e62479cb0cd1a4688832f3ace728f0e8029feb6f
Álvaro J <86482525+alvaro-j@users.noreply.github.com>
2021-08-30 16:04:23 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 var input = document.getElementsByClassName("input")[0];
+input.value = '';
 var input2 = document.getElementsByClassName("input")[1];
+input2.value = '';
+var bill = document.getElementById("bill");
+bill.addEventListener("input", setBillValue);
+var tipBtns = document.querySelectorAll('.span-percent')
+tipBtns.forEach(btn => {
+    btn.addEventListener('click', handleClick)
+})
 var btnReset = document.getElementById("btn-reset");
 
-input.value = "0";
-input2.value = "0";
+var billValue = 0.0;
+
+function handleClick() {
+    
+}
+
+function setBillValue() {
+    if (bill.value.includes(',')) {
+        bill.value = bill.value.replace(',' , '.');
+    }
+    billValue = parseFloat(bill.value);
+    console.log(billValue);
+}
+
