eaa99eaa88c6a52fdaf838c4e394573c09855d1b
Álvaro J <86482525+alvaro-j@users.noreply.github.com>
2021-08-30 14:40:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,6 @@
-var main = document.getElementsByTagName("MAIN")[0];
-main.addEventListener("onload", load());
-
-function load() {
-  var input = document.getElementsByClassName("input")[0];
-  var input2 = document.getElementsByClassName("input")[1];
-  var btnReset = document.getElementById("btn-reset");
-
-  main.style.backgroundColor = "red";
-  if (input.value != 0 && input2.value != 0) {
-    btnReset.style.backgroundColor = "red";
-  }
-}
-
-load();
+var input = document.getElementsByClassName("input")[0];
+var input2 = document.getElementsByClassName("input")[1];
+var btnReset = document.getElementById("btn-reset");
 
 input.value = "0";
 input2.value = "0";
