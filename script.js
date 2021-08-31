95dd0356c36ed88563ead3f185cde105554b8b91
Álvaro J <86482525+alvaro-j@users.noreply.github.com>
2021-08-31 14:40:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,14 +6,31 @@ var bill = document.getElementById("bill");
 bill.addEventListener("input", setBillValue);
 var tipBtns = document.querySelectorAll('.span-percent')
 tipBtns.forEach(btn => {
-    btn.addEventListener('click', handleClick)
+    btn.addEventListener('click', handleClick);
 })
+var tipCustom = document.querySelector('.span-custom');
+tipCustom.value = '';
+tipCustom.addEventListener('input', setTipCustomValue);
+var person = document.querySelector('.input-person');
+person.addEventListener('input', setPersonValue);
+
 var btnReset = document.getElementById("btn-reset");
 
 var billValue = 0.0;
+var tipValue = 0.15;
+var personValue = 1;
 
-function handleClick() {
-    
+function handleClick(event) {
+    tipBtns.forEach(btn => {
+        btn.classList.remove('btn-active');
+
+        if (event.target.innerHTML == btn.innerHTML) {
+            btn.classList.add('btn-active');
+            tipValue = parseFloat(btn.innerHTML)/100;
+        }
+    });
+    tipCustom.value = '';
+    console.log(tipValue);
 }
 
 function setBillValue() {
@@ -24,3 +41,17 @@ function setBillValue() {
     console.log(billValue);
 }
 
+function setTipCustomValue() {
+    tipValue = parseFloat(tipCustom.value/100);
+
+    tipBtns.forEach(btn => {
+        btn.classList.remove('btn-active');
+    });
+
+    console.log(tipValue);
+}
+
+function setPersonValue() {
+    personValue = parseFloat(person.value);
+    console.log(personValue);
+}
