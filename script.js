1b9255240211567ec5d5408cb9cae771d5c85c7e
Álvaro J <86482525+alvaro-j@users.noreply.github.com>
2021-09-05 10:07:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
-var input = document.getElementsByClassName("input")[0];
-input.value = '';
-var input2 = document.getElementsByClassName("input")[1];
-input2.value = '';
 var bill = document.getElementById("bill");
+bill.value = '';
 bill.addEventListener("input", setBillValue);
 var tipBtns = document.querySelectorAll('.span-percent')
 tipBtns.forEach(btn => {
@@ -12,12 +9,13 @@ var tipCustom = document.querySelector('.span-custom');
 tipCustom.value = '';
 tipCustom.addEventListener('input', setTipCustomValue);
 var person = document.querySelector('.input-person');
+person.value = '';
 person.addEventListener('input', setPersonValue);
 var errorMsg = document.querySelector('.error-msg');
-var results = document.querySelectorAll('.value');
-
+var results = document.querySelectorAll('.price-right');
 
 var btnReset = document.getElementById("btn-reset");
+btnReset.addEventListener('click', reset);
 
 var billValue = 0.0;
 var tipValue = 0.15;
@@ -33,7 +31,8 @@ function handleClick(event) {
         }
     });
     tipCustom.value = '';
-    console.log(tipValue);
+
+    calculateTip();
 }
 
 function setBillValue() {
@@ -53,7 +52,9 @@ function setTipCustomValue() {
         btn.classList.remove('btn-active');
     });
 
-    console.log(tipValue);
+    if (tipCustom.value !== '') {
+        calculateTip();
+    }
 }
 
 function setPersonValue() {
@@ -65,13 +66,25 @@ function setPersonValue() {
             errorMsg.classList.remove('show-error-msg');
         }, 3000);
     }
+    calculateTip();
 }
 
 function calculateTip() {
-    if (persoValue >= 1) {
+    if (personValue >= 1) {
         var tipAmount = billValue * tipValue / personValue;
         var total = billValue * (tipValue + 1) / personValue;
         results[0].innerHTML = '$' + tipAmount.toFixed(2);
-        results[1].innerHTML =  '$' + total.toFixed(2);
+        results[1].innerHTML = '$' + total.toFixed(2);
     }
+}
+
+function reset() {
+    bill.value = '';
+    setBillValue();
+
+    tipBtns[2].click();
+
+    person.value = '';
+    setPersonValue();
+
 }
